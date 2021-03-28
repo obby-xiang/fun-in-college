f514b861ec2edc2c7c079e5453443ea394b20710
obby-xiang <liuxiang.obby@qq.com>
2021-03-28 21:24:33 +0800

    RGB颜色

---
+++ Java程序设计/practice-java/src/main/java/org/example/RGBColor.java
@@ -0,0 +1,60 @@
+package org.example;
+
+/**
+ * @since 2016-11-03
+ */
+public class RGBColor {
+    private int color;//表示颜色的整数
+    public RGBColor(){
+        //无参数的构造方法
+        color=0xff000000;
+    }
+    public RGBColor(int color){
+        //以表示颜色的整数构造对象
+        this.set(color);
+    }
+    public RGBColor(int red, int green, int blue){
+        //以红、绿、蓝三原色值构造对象
+        this.set(red,green,blue);
+    }
+    public RGBColor(RGBColor color){
+        //以实例构造对象
+        set(color);
+    }
+    public void set(int color){
+        //以表示颜色的整数设置颜色值
+        this.color=color>=0?color:0xff000000;//如果整数值不在范围内，取值0xff000000
+    }
+    public void set(int red,int green,int blue){
+        //以红、蓝、绿三原色值设置颜色值
+        red=red>=0&&red<=0xff?red:0;
+        green=green>=0&&green<=0xff?green:0;
+        blue=blue>=0&&blue<=0xff?blue:0;//如果单色值不在[0,255]的范围内，取值为0
+        color=0xff000000+red*0x10000+green*0x100+blue;
+    }
+    public void set(RGBColor color){
+        //以实例设置颜色值
+        set(color.color);
+    }
+    public int getColor(){
+        //获得表示颜色的整数
+        return color;
+    }
+    public String toString(){
+        //返回表示颜色的整数值和RGB值
+        int color=this.color>=0xff000000?this.color-0xff000000:this.color;
+        return "Color 0x"+Integer.toHexString(this.color)+
+                " RGB("+color/0x10000%0x100+","+color/0x100%0x100+","+color%0x100+")";
+    }
+    public static void main(String args[]){
+        RGBColor x=new RGBColor(2,5,6);
+        System.out.println(x.toString());
+        x.set(256);
+        System.out.println(x.toString());
+    }
+}
+/*
+程序运行结果:
+Color 0xff020506 RGB(2,5,6)
+Color 0x100 RGB(0,1,0)
+*/
